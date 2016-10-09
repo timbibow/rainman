11efeff365a5cb3246793875e5cedb402e3cb578
timbibow <tbibow@gmail.com>
2016-10-09 16:55:02 -0400

    Update index.js

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,5 +1,16 @@
 'use strict'
 
+const firstOfEntityRole = function(message, entity, role) {
+  role = role || 'generic';
+
+  const slots = message.slots
+  const entityValues = message.slots[entity]
+  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+
+  return valsForRole ? valsForRole[0] : null
+}
+
+
 exports.handle = function handle(client) {
 
   const sayHello = client.createStep({
@@ -35,10 +46,22 @@ const collectCity = client.createStep({
   satisfied() {
     return Boolean(client.getConversationState().weatherCity)
   },
+	
+	
+	  extractInfo() {
+    const city = firstOfEntityRole(client.getMessagePart(), 'city')
 
-  prompt() {
-    // Need to prompt user for city    
-    console.log('Need to ask user for city')
+    if (city) {
+      client.updateConversationState({
+        weatherCity: city,
+      })
+
+      console.log('User wants the weather in:', city.value)
+    }
+  },
+
+prompt() {
+    client.addResponse('app:response:name:prompt/weather_city')
     client.done()
   },
 })
